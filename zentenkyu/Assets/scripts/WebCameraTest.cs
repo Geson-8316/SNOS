d32547794e47e3fde7f5bdaabae5091c018cb2d0
Geson-8316 <59086897+Geson-8316@users.noreply.github.com>
2019-12-28 10:02:19 +0900

    既存
    とりあえず上げといたお

---
+++ zentenkyu/Assets/scripts/WebCameraTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCameraTest : MonoBehaviour
+{
+
+
+    WebCamTexture webCamTexture;
+
+    void Start()
+    {
+        GameObject sphere = GameObject.Find("Sphere");
+        WebCamControll sphereScript = sphere.GetComponent<WebCamControll>();
+
+        webCamTexture = sphereScript.webcamTexture;
+        GetComponent<Renderer>().material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+    void Update()
+    {
+    }
+}
